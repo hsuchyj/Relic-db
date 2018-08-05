810ba40f1449706748f1f7a5c5fef8d18d2d282e
= <=>
2018-08-05 15:16:03 -0400

    removed unnecessary home files and php files.

--- my_app/controllers/home.py
+++ my_app/controllers/home.py
@@ -11,32 +11,43 @@ from bson import json_util
 from pymongo import MongoClient
 from tkinter import filedialog
 from tkinter import *
+from flask import render_template
+from run_server import app
 
 @app.route("/home")
 def mainPage():
     return render_template("searchBar.html")
 
 
-#add param for different lti i.e. canvas, respondus etc
+# add param for different lti i.e. canvas, respondus etc
 @app.route("/import")
 def importQuestions():
     client = MongoClient('localhost', 27017)
 
+    # user selects file from gui
 
-    #user selects file from gui
-    
     root = Tk()
-    root.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file")
+    root.filename = filedialog.askopenfilename(initialdir="/", title="Select file")
     xmlLoc = root.filename
     with open(xmlLoc, encoding='UTF-8') as fd:
         doc = xmltodict.parse(fd.read())
         jsonDoc = json_util.loads(json.dumps(doc))
 
-    #for canvas qti only
-    
+    # for canvas qti only
+
     itemsArray = jsonDoc['questestinterop']['assessment']['section']['section'][0]['item']
     for doc in itemsArray:
         questions.insert(doc)
 
-    #questions.insert(newDoc)
-    #client.close()
+        # questions.insert(newDoc)
+        # client.close()
+
+def setup_logging(logging_path, level):
+    '''Setups logging in app'''
+    from logging.handlers import RotatingFileHandler
+    from logging import getLogger, getLevelName
+    file_handler = RotatingFileHandler(logging_path)
+    file_handler.setLevel(getLevelName(level))
+    loggers = [app.logger]
+    for logger in loggers:
+        logger.addHandler(file_handler)
--- run_server.py
+++ run_server.py
@@ -1,26 +1,17 @@
 from my_app.config import settings, assets
-
 from flask import Flask
-
 # set up the flask app
 
 app = Flask(__name__, static_folder= "my_app/static",
             template_folder="my_app/templates")
 assets.init_assets(app)
+from my_app.controllers.home import *
 
 
-def setup_logging(logging_path, level):
-    '''Setups logging in app'''
-    from logging.handlers import RotatingFileHandler
-    from logging import getLogger, getLevelName
-    file_handler = RotatingFileHandler(logging_path)
-    file_handler.setLevel(getLevelName(level))
-    loggers = [app.logger]
-    for logger in loggers:
-        logger.addHandler(file_handler)
+
+
 
 if __name__ == "__main__":
-
     app.run(debug=app.debug, port=5001)
     setup_logging(logging_path=settings.LOGGING_PATH,
                   level=settings.LOGGING_LEVEL)
