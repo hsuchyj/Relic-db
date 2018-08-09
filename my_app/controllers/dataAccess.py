e1792c6516f39770573fc2189c84501a348ebd0c
= <=>
2018-08-09 12:04:38 -0400

    added dataAccess layer.

---
+++ my_app/controllers/dataAccess.py
@@ -0,0 +1,80 @@
+
+import json
+from ..config import settings
+from os.path import join
+from ..scripts import scripts
+
+import xmltodict
+import pprint
+from io import open
+from bson import json_util
+from bson.json_util import dumps
+from pymongo import MongoClient
+from tkinter import filedialog
+from tkinter import *
+import sys
+def importQuestions():
+    raise Exception("TODO Not fully implemented")
+    """
+    client = MongoClient('localhost', 27017)
+    db = client['relic']
+    questions = db['questions']
+    # user selects file from gui
+    # root = Tk()
+    # root.filename = filedialog.askopenfilename(initialdir="/", title="Select file")
+    xmlLoc = "C:\\Users\\OG AppleJacks\\Documents\\cisc106rework\\staging-mattsap-quiz-export\\i682d359907261533e3507b8d0b191d73\\i682d359907261533e3507b8d0b191d73.xml"
+    with open(xmlLoc, encoding='UTF-8') as fd:
+        doc = xmltodict.parse(fd.read())
+        jsonDoc = json_util.loads(json.dumps(doc))
+    # for canvas qti only
+    itemsArray = jsonDoc['questestinterop']['assessment']['section']['section'][0]['item']
+    for doc in itemsArray:
+        questions.insert(doc)
+
+        # questions.insert(newDoc)
+    client.close()
+    print("imported stuff")
+    """
+
+def getTags():
+    """
+    client = MongoClient('localhost', 27017)
+    db = client['relic']
+    tags = db['tags']
+    select = request.args.get('tagFilter')
+    # have to change 'function' to whatever the first entry for that tag is
+    tagData = tags.find({select :{'$exists' :True}})
+    for items in tagData:
+        tagList = items[select]
+    return jsonify(result = tagList)
+    """
+    return {
+   "units":[
+      {
+         "unit_name":"Simple Python Data",
+         "unit_SLO":"Write a script/program that asks a user for input values from the keyboard, performs a mathematical computation and displays the result to the screen.",
+         "topics":[
+            "operators",
+            "variables"
+         ],
+         "skills":[
+            {
+               "skill_name":"algorithms",
+               "skill_slos":[
+                  "write algorithm for solving simple mathematical problems",
+                  "understand the difference between an algorithm and a program"
+               ]
+            },
+            {
+               "skill_name":"operators",
+               "skill_slos":[
+                  "evaluate expressions containing these operators",
+                  "understand the difference beween floating point and integer division",
+                  "convert a mathematical formula into a Python expression"
+               ]
+            }
+         ]
+      }
+   ]
+}
+
--- my_app/controllers/home.py
+++ my_app/controllers/home.py
@@ -1,60 +1,20 @@
-
-import json
-from ..config import settings
-from os.path import join
-from ..scripts import scripts
-
-import xmltodict
-import pprint
-from io import open
-from bson import json_util
-from bson.json_util import dumps
-from pymongo import MongoClient
-from tkinter import filedialog
-from tkinter import *
 from flask import render_template, request, jsonify
 from run_server import app
-import sys
-
+import my_app.controllers.dataAccess as dataAccess
 
 @app.route("/home")
 def mainPage():
     return render_template("searchBar.html")
 
-
 # add param for different lti i.e. canvas, respondus etc
 @app.route("/import")
 def importQuestions():
-    client = MongoClient('localhost', 27017)
-    db = client['relic']
-    questions = db['questions']
-    # user selects file from gui
-    #root = Tk()
-    #root.filename = filedialog.askopenfilename(initialdir="/", title="Select file")
-    xmlLoc = "C:\\Users\\OG AppleJacks\\Documents\\cisc106rework\\staging-mattsap-quiz-export\\i682d359907261533e3507b8d0b191d73\\i682d359907261533e3507b8d0b191d73.xml"
-    with open(xmlLoc, encoding='UTF-8') as fd:
-        doc = xmltodict.parse(fd.read())
-        jsonDoc = json_util.loads(json.dumps(doc))
-    # for canvas qti only
-    itemsArray = jsonDoc['questestinterop']['assessment']['section']['section'][0]['item']
-    for doc in itemsArray:
-        questions.insert(doc)
-
-        # questions.insert(newDoc)
-    client.close()
-    print("imported stuff")
+    return dataAccess.importQuestions()
 
 @app.route("/getTagsFromDB", methods = ["GET"])
 def getTagsFromDB():
-    client = MongoClient('localhost', 27017)
-    db = client['relic']
-    tags = db['tags']
-    select = request.args.get('tagFilter')
-    #have to change 'function' to whatever the first entry for that tag is
-    tagData = tags.find({select:{'$exists':True}})
-    for items in tagData:
-        tagList = items[select]
-    return jsonify(result = tagList)
+    return dataAccess.getTags()
+
 
 
 def setup_logging(logging_path, level):
